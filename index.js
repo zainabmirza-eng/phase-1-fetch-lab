2c21a1c0abb03e684afb1d567826d8c66ab96a0f
zainab mirza <zainabmirzafatah99@gmail.com>
2024-05-25 17:34:52 +0300

    added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+    
 }
 
 function renderBooks(books) {
